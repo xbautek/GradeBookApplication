da272d0a9cb3bdee3e4da3c2a887cea75196c1ea
xbautek <bartosz.ksiazek@microsoft.wsei.edu.pl>
2023-03-14 20:29:16 +0100

    6 tests passed

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
